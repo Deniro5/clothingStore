753475ba264495fa458ad2670bc0bdcadb1828ad
USER <user@USERs-MacBook-Pro.local>
2020-03-05 03:39:31 -0500

    Updates

---
+++ precache-manifest.7656aca27fde4de62173a260f0716eca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/clothingStore/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/clothingStore/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/clothingStore/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "794cb80c756da411d890",
+    "url": "/clothingStore/static/js/runtime~main.94cf2f85.js"
+  },
+  {
+    "revision": "9c474340474a852982d1",
+    "url": "/clothingStore/static/js/main.8d9c5ddb.chunk.js"
+  },
+  {
+    "revision": "e797d4451121491759f4",
+    "url": "/clothingStore/static/js/2.362fa241.chunk.js"
+  },
+  {
+    "revision": "9c474340474a852982d1",
+    "url": "/clothingStore/static/css/main.233f6d6b.chunk.css"
+  },
+  {
+    "revision": "e797d4451121491759f4",
+    "url": "/clothingStore/static/css/2.b8088515.chunk.css"
+  },
+  {
+    "revision": "997778667a70680cb6b97302aeeb8bcf",
+    "url": "/clothingStore/index.html"
+  }
+];

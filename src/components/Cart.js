902e13f1309db1bc1204f94c3554089fccd9a65e
USER <user@USERs-MacBook-Pro.local>
2020-04-30 01:47:14 -0400

    minor adjustment to cart size

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,49 +1,65 @@
-import React, { useContext, Fragment } from 'react';
-import {CartContext} from './CartContext'
+import React, { useContext } from 'react';
+import { CartContext } from './CartContext';
 
 const Cart = props => {
+  const [cart, setCart] = useContext(CartContext);
 
-  const [cart, setCart] = useContext(CartContext)
-
-  const confirmDelete = (id) => {    //use the imgsrc as the id since they
-    const choice = window.confirm("Are you sure you want to remove this item from your cart?");
+  const confirmDelete = id => {
+    //use the imgsrc as the id since they
+    const choice = window.confirm(
+      'Are you sure you want to remove this item from your cart?'
+    );
     if (choice) {
-      let oldCart = [...cart]
+      let oldCart = [...cart];
       let index = 0;
       while (index < cart.length) {
         if (id === cart[index].id) {
           oldCart.splice(index, 1);
-          break
+          break;
         }
-        index++
+        index++;
       }
-      setCart(oldCart)
+      setCart(oldCart);
     }
-  }
+  };
 
-    return (
-      <div style = {{textAlign:"center"}}>
-      <h2 style = {{marginTop:"25px"}}>Your Cart</h2>   
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2 style={{ marginTop: '25px' }}>Your Cart</h2>
       <div className="cartContainer">
-        {cart.length > 0 ? cart.map((product) => (
-          <div className = "cartResultsSegment">
-            <img alt = "placeholder" src = "imgs/close.png" className="cartItemDelete" onClick = {confirmDelete.bind(this, product.id)}/>
-            <img alt = "productImg" src = {product.imgsrc} className="cartResultsSegmentImg"/>
-            <p className="cartResultsSegmentName">
-              {product.name}
-            </p>
-            <p className="cartResultsSegmentInfo">
-              Price: {product.price} <span style = {{marginLeft:"15px"}}> Size: {product.size} </span> <span style = {{marginLeft:"15px"}}> Style: {product.style} </span>
-            </p>
-            
-          </div>
-        ))
-      : 
-      <p> No items currently in cart</p>}
+        {cart.length > 0 ? (
+          cart.map(product => (
+            <div className="cartResultsSegment">
+              <img
+                alt="placeholder"
+                src="imgs/close.png"
+                className="cartItemDelete"
+                onClick={confirmDelete.bind(this, product.id)}
+              />
+              <img
+                alt="productImg"
+                src={product.imgsrc}
+                className="cartResultsSegmentImg"
+              />
+              <p className="cartResultsSegmentName">{product.name}</p>
+              <p className="cartResultsSegmentInfo">
+                Price: {product.price}
+                <span style={{ marginLeft: '15px' }}>Size: {product.size}</span>
+                <span style={{ marginLeft: '15px' }}>
+                  Style: {product.style}
+                </span>
+              </p>
+            </div>
+          ))
+        ) : (
+          <p> No items currently in cart</p>
+        )}
       </div>
-      <button onClick = {props.handleClose} className = "cartButton"> Continue Shopping </button>
-       </div>
-    );
-  }
+      <button onClick={props.handleClose} className="cartButton">
+        Continue Shopping
+      </button>
+    </div>
+  );
+};
 
 export default Cart;
